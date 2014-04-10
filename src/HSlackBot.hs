5f57bfdd8d23dc2a6955450e82b96fd773a70d91
David Tchepak <dave@davesquared.net>
2014-04-10 11:52:37 +1000

    Parsing jira issue references

--- src/HSlackBot.hs
+++ src/HSlackBot.hs
@@ -3,7 +3,15 @@ module HSlackBot where
 
 import Control.Applicative
 import Data.Aeson (ToJSON(..), (.=), object)
+import Data.Char (isAlphaNum)
+import Data.List (intercalate)
+import Data.Maybe (catMaybes)
 import Data.Monoid (mconcat)
+import qualified Text.Parsec as P
+import qualified Text.Parsec.String as P
+import Text.Parser.Char
+import Text.Parser.Combinators
+import Text.Parser.Token
 import Web.Scotty
 
 type Port = Int
@@ -22,15 +30,15 @@ text=googlebot: What is the air-speed velocity of an unladen swallow?
 trigger_word=googlebot:
 -}
 data SlackRequest = SlackRequest
-    { token         :: String
-    , team_id       :: String
-    , channel_id    :: String
-    , channel_name  :: String
-    , timestamp     :: String
-    , user_id       :: String
-    , user_name     :: String
-    , msg_text      :: String
-    , trigger_word  :: String
+    { token       :: String
+    , teamId      :: String
+    , channelId   :: String
+    , channelName :: String
+    , timestamp   :: String
+    , userId      :: String
+    , userName    :: String
+    , msgText     :: String
+    , triggerWord :: String
     }
 
 {- Response:
@@ -41,12 +49,16 @@ instance ToJSON SlackResponse where
   toJSON (SlackResponse resp) = 
     object [ "text" .= resp ]
 
+type JiraPath = String
+type JiraProject = String
+type JiraIssue = String
+
 start :: Port -> IO ()
 start port = scotty port $ do
-    get "/" $ do
-        html "hello world"
+    get "/" $ html "hello world"
     post "/jira" $ do
         jiraPath    <- param "jiraPath"
+        jiraProjs   <- param "jiraProjects"
         req         <- SlackRequest
                         <$> param "token"
                         <*> param "team_id"
@@ -57,14 +69,34 @@ start port = scotty port $ do
                         <*> param "user_name"
                         <*> param "text"
                         <*> param "trigger_word"
+        maybe (json $ SlackResponse "hi") json $
+            proj `parse` jiraProjs >>= \p -> jiraResponder jiraPath p req
 
-        json $ SlackResponse $ mconcat 
-                [ trigger_word req
-                , "\n"
-                , jiraPath
-                , "\n"
-                , msg_text req
-                ]
+parse :: P.Parser a -> String -> Maybe a
+parse p = either (const Nothing) Just . P.parse p []
 
-jiraResponder :: SlackRequest -> Maybe SlackResponse
-jiraResponder = undefined
+proj :: TokenParsing m => m [JiraProject]
+proj = commaSep (some upper)
+
+issueParser :: (Monad m, TokenParsing m) => [JiraProject] -> m JiraIssue
+issueParser projs = do
+    ref <- choice (string <$> projs)
+    char '-'
+    num <- natural
+    return $ ref ++ '-' : show num
+
+issuesParser :: (Monad m, TokenParsing m) => [JiraProject] -> m [JiraIssue]
+issuesParser projs =
+    let refs = Just <$> issueParser projs
+    in catMaybes <$> many (try refs <|> (anyChar *> return Nothing))
+
+jiraResponder ::
+    JiraPath
+    -> [JiraProject]
+    -> SlackRequest
+    -> Maybe SlackResponse
+jiraResponder path projs req =
+    let toMsg = intercalate "\n"
+        issuesToPath = fmap (path++)
+        respP = (SlackResponse . toMsg . issuesToPath) <$> issuesParser projs
+    in respP `parse` msgText req
--- test/testRequest.sh
+++ test/testRequest.sh
@@ -8,7 +8,7 @@ curl \
     -d "timestamp=1355517523.000005" \
     -d "user_id=U2147483697" \
     -d "user_name=Steve" \
-    -d "text=googlebot: What is the air-speed velocity of an unladen swallow?" \
+    -d "text=googlebot: look at ABC-123, DEF-456 and OTHER-03" \
     -d "trigger_word=googlebot:" \
-    "http://localhost:8080/jira?jiraPath=myserver/display"
+    "http://localhost:8080/jira?jiraPath=myserver/display&jiraProjects=ABC,DEF"
 
